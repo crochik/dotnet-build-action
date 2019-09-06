60be8dd663ecec1b9cdb8eb9300257da33ef3c70
Felipe Crochik <felipe@crochik.com>
2019-09-05 21:58:55 -0400

    1.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
+set -e
+set -o pipefail
 
-echo "Second"
-echo ${GITHUB_WORKSPACE}
-echo ${INPUT_PROJECT_PATH}
-
-ls ${INPUT_PROJECT_PATH}
+echo "Building: ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH}"
 
 # docker build -f dotnet.Dockerfile ${INPUT_BUILD_CONTEXT}
 dotnet publish ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH} -c Debug
