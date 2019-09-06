31717d651d96fcf836a5147f1c905e1f96e9f85f
Felipe Crochik <felipe@crochik.com>
2019-09-05 22:01:42 -0400

    disable telemetry

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,7 @@ set -o pipefail
 
 echo "Building: ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH}"
 
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
 # docker build -f dotnet.Dockerfile ${INPUT_BUILD_CONTEXT}
 dotnet publish ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH} -c Debug
