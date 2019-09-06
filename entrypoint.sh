7a6297e8a35de9eeee8bba07439cf8f5f78bf662
Felipe Crochik <felipe@crochik.com>
2019-09-05 22:07:30 -0400

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-set -o pipefail
 
 echo "Building: ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH}"
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 # docker build -f dotnet.Dockerfile ${INPUT_BUILD_CONTEXT}
-dotnet publish ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH} -c Debug
+dotnet publish ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH} -o ${GITHUB_WORKSPACE}/build -c Debug
+find ${GITHUB_WORKSPACE}/build
