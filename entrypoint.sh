f6b4ef6646822231430a5bc68c2edf4aa4ce2075
Felipe Crochik <felipe@crochik.com>
2019-10-15 11:42:51 -0400

    relative path

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,14 @@ echo "Building: ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH}"
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
-# docker build -f dotnet.Dockerfile ${INPUT_BUILD_CONTEXT}
-dotnet publish ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH} -o ${GITHUB_WORKSPACE}/build -c Debug
-dotnet pack
-
 nuget sources Add -Name "Github" \
      -Source "https://nuget.pkg.github.com/crochik/index.json" \
      -UserName crochik -Password ${GITHUB_TOKEN} \
 
-nuget push ${GITHUB_WORKSPACE}/bin/Debug/Crochik.NET.1.0.0.nupkg -Source "Github"
+# docker build -f dotnet.Dockerfile ${INPUT_BUILD_CONTEXT}
+dotnet publish ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH} -o ${GITHUB_WORKSPACE}/build -c Debug
+dotnet pack
+
+nuget push bin/Debug/Crochik.NET.1.0.0.nupkg -Source "Github"
 
 find ${GITHUB_WORKSPACE}/build
