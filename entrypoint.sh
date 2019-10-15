baa205dc9eb5f227d6fd6bd1ffcbd04d31fa512d
Felipe Crochik <felipe@crochik.com>
2019-10-15 11:23:56 -0400

    pack and add nuget source

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,10 @@ export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 # docker build -f dotnet.Dockerfile ${INPUT_BUILD_CONTEXT}
 dotnet publish ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH} -o ${GITHUB_WORKSPACE}/build -c Debug
+dotnet pack
+
+nuget sources Add -Name "Github" \
+     -Source "https://nuget.pkg.github.com/crochik/index.json" \
+     -UserName crochik -Password ${GITHUB_TOKEN} \
+
 find ${GITHUB_WORKSPACE}/build
