f045f309908232f57892e2a4a642b4e7a33e463e
Felipe Crochik <felipe@crochik.com>
2019-10-16 09:49:15 -0400

    INPUT_CONFIGURATION

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ echo "Building: ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH}"
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
-dotnet publish ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH} -o ${GITHUB_WORKSPACE}/build -c Debug
+dotnet publish ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH} -o ${GITHUB_WORKSPACE}/build -c ${INPUT_CONFIGURATION}
 dotnet pack
-dotnet push bin/Debug/Crochik.NET.1.0.1.nupkg -Source "crochik"
+dotnet nuget push bin/${INPUT_CONFIGURATION}/*.nupkg -s "Github"
 
 find ${GITHUB_WORKSPACE}/build
