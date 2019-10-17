dbf697728ca72410d7da460e48a052fea7e60746
Felipe Crochik <felipe@crochik.com>
2019-10-17 14:08:55 -0400

    auto replace GITHUB_TOKEN

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # First Action
+
+## Expects
+* GITHUB_WORKSPACE
+* GITHUB_TOKEN (optional)
+
+## Inputs
+* PROJECT_PATH
+* SOURCE: 
+* CONFIGURATION: Debug or Release
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,20 @@ set -e
 
 echo "Building: ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH}"
 
+if [[ -f "nuget.config" && -n "$GITHUB_TOKEN" ]]
+then
+    echo "Injecting GITHUB_TOKEN in nuget.config..."
+    REGEX="s/\${{GITHUB_TOKEN}}/${GITHUB_TOKEN}/"
+    sed -i '' $REGEX nuget.config
+fi
+
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 dotnet publish ${GITHUB_WORKSPACE}/${INPUT_PROJECT_PATH} -o ${GITHUB_WORKSPACE}/build -c ${INPUT_CONFIGURATION}
 dotnet pack
-dotnet nuget push bin/${INPUT_CONFIGURATION}/*.nupkg -s "Github"
 
-find ${GITHUB_WORKSPACE}/build
+if [[ -n "$INPUT_SOURCE" ]]
+then 
+    echo "Publishing to NuGet: ${INPUT_SOURCE}..."
+    dotnet nuget push bin/${INPUT_CONFIGURATION}/*.nupkg -s ${INPUT_SOURCE}
+fi
